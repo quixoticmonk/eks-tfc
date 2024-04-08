33c3ffa0462ae896616ad522787dc92802fc70e7
quixoticmonk <manuchandrasekhar@gmail.com>
2024-04-07 22:39:55 -0400

    fix: set to 1.8 of NR bundle

--- main.tf
+++ main.tf
@@ -261,5 +261,5 @@ resource "aws_eks_addon" "newrelic_addon" {
   depends_on    = [module.eks]
   addon_name    = "new-relic_kubernetes-operator"
   cluster_name  = module.eks.cluster_name
-  addon_version = "v0.1.9-eksbuild.1"
+  addon_version = "v0.1.8-eksbuild.1"
 }

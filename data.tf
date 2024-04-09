420497b9dfd92c284aa1cbcc6b466ccd0f08a75f
quixoticmonk <manuchandrasekhar@gmail.com>
2024-04-09 16:06:08 -0400

    test: datasource for postconditions

--- data.tf
+++ data.tf
@@ -20,3 +20,19 @@ data "aws_eks_addon_version" "nr" {
   addon_name         = "new-relic_kubernetes-operator"
   kubernetes_version = local.cluster_version
 }
+
+data "aws_eks_cluster" "cluster" {
+  name= module.eks.cluster_name
+  lifecycle {
+    postcondition {
+      condition = self.cluster_version==local.cluster_version
+      error_message = "The cluster version is set to ${self.cluster_version}"
+    }
+        postcondition {
+      condition = self.cluster_name==local.name
+      error_message = "The cluster version is set to ${self.cluster_name}"
+    }
+  }
+
+
+}
